0763a28e73920b5bc61852044d1c7157b86baac8
Ricardo Adelino <adelinojrp@gmail.com>
2026-03-20 17:05:36 -0300

    Atualização de dados

--- teste.r
+++ teste.r
@@ -3,6 +3,9 @@ library(tidyverse)
 library(sf)
 library(terra)
 
+#db <- readRDS("/media/ricardo/Backup2/Observatorio/Pacote/db_/ocorrencias.rds")
+#save(db,file = "data/db_oco.rda")
+
 load("data/db_splist.rda")
 load("data/db_oco.rda")
 load("data/geometrias.rda")
@@ -13,3 +16,4 @@ sp <- db_oco %>% filter(especie_ajustado %in% vt)
 #bacia <- geometrias %>% filter(class == "EcoRegions") %>% vect()
 ecor <- geometrias %>% filter(class == "EcoRegions") %>% vect()
 ecor$nome
+
